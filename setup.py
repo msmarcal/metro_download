2f167bd79cb5107083f630cae3c19b0fab38b54f
Marcelo Marcal <marcelo@smarcal.com>
2019-02-26 14:29:53 -0300

    Added author details

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(
     name='metro_download',
     version='0.0.1',
+    description='Downloads Brazilian Metro newspaper.',
+    author='Marcelo Subtil Marcal',
+    author_email='marcelo@smarcal.com',
+    license='MIT',
     py_modules=['metro'],
     install_requires=[
         'datetime',
